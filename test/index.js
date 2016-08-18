3370d82f7c9dc587fe6fb929429a85b1e7ef94fc
Mustafa Dokumaci <mustafa@foraa.com>
2016-08-18 12:34:49 +0200

    Initial cleanup

--- test/index.js
+++ test/index.js
@@ -1,11 +1,2 @@
 'use strict'
-const test = require('tape')
-const proxy = require('../')
-const BriskyServer = require('../')
-const http = require('http')
 
-test('brisky server - serve a build', (t) => {
-  t.plan(2)
-  // test slack, travis and getting files
-  t.end()
-})
