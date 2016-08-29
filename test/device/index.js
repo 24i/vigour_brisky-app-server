2b9305e0ca1114f82d051d9a1be1330e447f54a8
Jim de Beer <jim@vigour.io>
2016-08-29 11:00:45 +0200

    better test

--- test/device/index.js
+++ test/device/index.js
@@ -3,50 +3,70 @@ const server = require('../../lib/server')
 const path = require('path')
 const http = require('http')
 const test = require('tape')
+const fs = require('fs')
 
-test('app server - get phone index', t => {
-  t.plan(2)
-  var appServer = server(9090, path.resolve(__dirname))
+test('app server - get device index', t => {
+  t.plan(5)
+  const appServer = server(9090, path.resolve(__dirname))
+  const host = 'localhost'
 
-  require('dns').lookup(require('os').hostname(), (err, add, fam) => {
-    var options = {
-      host: add,
-      port: 9090,
-      path: '/slurp',
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (SMART-TV; Linux; Tizen 2.3) AppleWebkit/538.1 (KHTML, like Gecko) SamsungBrowser/1.0 TV Safari/538.1'
-      }
+  http.get({
+    host,
+    port: 9090,
+    path: '/slurp',
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (SMART-TV; Linux; Tizen 2.3) AppleWebkit/538.1 (KHTML, like Gecko) SamsungBrowser/1.0 TV Safari/538.1'
     }
-    http.get(options, (res) => {
-      t.equal(res.statusCode, 200, 'should always return 200')
-      res.resume()
-    }).on('error', (e) => {
-      console.log(`Got error: ${e.message}`)
+  }, (res) => {
+    t.equal(res.statusCode, 200, 'should always return 200')
+    var data = ''
+    res.on('data', (chunk) => {
+      data += chunk
     })
-
-    options = {
-      host: add,
-      port: 9090,
-      path: '/bundle.css',
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (SMART-TV; Linux; Tizen 2.3) AppleWebkit/538.1 (KHTML, like Gecko) SamsungBrowser/1.0 TV Safari/538.1'
-      }
-    }
-    http.get(options, (res) => {
-      let body = ''
-      res.on('data', function (d) {
-        body += d
-      })
-      res.on('end', function () {
-        const expected = 'body { background-image: url("http://urbanislandz.com/wp-content/uploads/2016/08/Young-Thug-dress-bonnet.jpg"); }'
-        t.equal(expected, body, 'should load tv .css')
-        res.resume()
-      })
-    }).on('error', (e) => {
-      console.log(`Got error: ${e.message}`)
+    res.on('end', () => {
+      t.equal(data, fs.readFileSync(path.join(__dirname, 'tv/index.html')).toString(), 'loads index.html for tv')
     })
-    if (err) {}
+  }).on('error', (e) => {
+    console.log(`Got error: ${e.message}`)
   })
+
+  http.get({
+    host,
+    port: 9090
+  }, (res) => {
+    t.equal(res.statusCode, 200, 'should always return 200')
+    var data = ''
+    res.on('data', (chunk) => {
+      data += chunk
+    })
+    res.on('end', () => {
+      t.equal(data, fs.readFileSync(path.join(__dirname, 'default/index.html')).toString(), 'loads index.html for default')
+    })
+  }).on('error', (e) => {
+    console.log(`Got error: ${e.message}`)
+  })
+
+  http.get({
+    host,
+    port: 9090,
+    path: '/bundle.css',
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (SMART-TV; Linux; Tizen 2.3) AppleWebkit/538.1 (KHTML, like Gecko) SamsungBrowser/1.0 TV Safari/538.1'
+    }
+  }, (res) => {
+    let body = ''
+    res.on('data', function (d) {
+      body += d
+    })
+    res.on('end', function () {
+      const expected = 'body { background-image: url("http://urbanislandz.com/wp-content/uploads/2016/08/Young-Thug-dress-bonnet.jpg"); }'
+      t.equal(expected, body, 'should load tv .css')
+      res.resume()
+    })
+  }).on('error', (e) => {
+    console.log(`Got error: ${e.message}`)
+  })
+
   test.onFinish(() => {
     appServer.close()
   })
